5cdf25ac23001d7842fa8fd89583ab50c34b1764
Saad Shoaib <binshoaibsaad@gmail.com>
2024-05-28 19:59:16 +0500

    1

---
+++ Restaurant-Portfolio-Website/src/constants/images.js
@@ -0,0 +1,32 @@
+import G from '../assets/G.png';
+
+import knife from '../assets/knife.png';
+import logo from '../assets/logo.png';
+import overlaybg from '../assets/overlaybg.png';
+import spoon from '../assets/spoon.svg';
+import welcome from '../assets/welcome.png';
+import laurels from '../assets/laurels.png';
+import award01 from '../assets/award01.png';
+import award02 from '../assets/award02.png';
+import award03 from '../assets/award03.png';
+import award05 from '../assets/award05.png';
+import sign from '../assets/sign.png';
+import quote from '../assets/quote.png';
+import gericht from '../assets/gericht.png';
+
+export default {
+  G,
+  knife,
+  logo,
+  overlaybg,
+  spoon,
+  welcome,
+  laurels,
+  award01,
+  award02,
+  award03,
+  award05,
+  sign,
+  quote,
+  gericht,
+};
